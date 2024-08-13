8efb772a3adfadf27b5908ca824e04636f69a762
Aylin Abdollahi <ailinabdollahi113@gmail.com>
2024-08-13 11:25:00 +0330

    cv

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Menu = () => (
     <p><a href='#services'>Services</a></p>
     <p><a href='#about'>About</a></p>
     
-    <p><a href='https://drive.google.com/file/d/1QDqnl9HvnNNSbP7tL59JWF5DuNVRBFEK/view?usp=sharing' target="_blank">Download CV</a></p>
+    <p><a href='' target="_blank">Download CV</a></p>
     </>
 )
 
