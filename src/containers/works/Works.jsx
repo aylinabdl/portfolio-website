ad0983b94625c214ebc69061076c759f05be289b
Aylin Abdollahi <ailinabdollahi113@gmail.com>
2024-08-19 14:03:28 +0330

    work update

--- src/containers/works/Works.jsx
+++ src/containers/works/Works.jsx
@@ -14,17 +14,17 @@ function Works() {
         <h1 className='gradient-text'>Portfolio</h1>
         <h3>Check Out Some of My Projects.</h3>
             <div className='works-container'>
-                <Work image={dAP} title="Doctor Appointment App" subTitle="Case Study(still not complete)" text=""  />
+                <Work image={dAP} title="Doctor Appointment App" subTitle="Case Study" text="Simplifies booking and managing doctor appointments with reminders and secure scheduling."  />
 
-                <Work image={Travel} title="Travel Website" subTitle="UI/UX Design" text="" codeLink="https://www.figma.com/design/KdjjGgklTNUwbV8P58ij49/Travel?node-id=0-35&t=PGf2yKRfliAv70EX-1" />
+                <Work image={Travel} title="Travel Website" subTitle="UI/UX Design" text="Find and book exciting tours around the world, perfect for your next adventure." codeLink="https://www.figma.com/design/KdjjGgklTNUwbV8P58ij49/Travel?node-id=0-35&t=PGf2yKRfliAv70EX-1" />
 
-                <Work image={MovieApp} title="MovieNet" subTitle="UI Design & Development" text="" codeLink="https://www.figma.com/design/f0XaIW7rwN7PJPbF8S2yPC/Movie-App?node-id=0-1&t=wVDocizjjMihQLdm-1" liveLink="https://movienet-project.netlify.app/" />
+                <Work image={MovieApp} title="MovieNet" subTitle="UI Design & Development" text="Discover and track movies and TV series with personalized recommendations." codeLink="https://www.figma.com/design/f0XaIW7rwN7PJPbF8S2yPC/Movie-App?node-id=0-1&t=wVDocizjjMihQLdm-1" liveLink="https://movienet-project.netlify.app/" />
 
-                {/*<Work image={ProfileApp} title="Finance App" subTitle="UI/UX Design" text="" codeLink="https://www.figma.com/design/IDdYqPGerrqKy6SMhky1wS/Untitled?node-id=0-1&t=iDiS56eZiTqNwXOF-1" />
+                {/*<Work image={Taskies} title="Taskies" subTitle="UI/UX Design" text="Organize tasks and projects, set priorities, and collaborate efficiently to achieve your goals." codeLink="https://www.figma.com/design/IDdYqPGerrqKy6SMhky1wS/Untitled?node-id=0-1&t=iDiS56eZiTqNwXOF-1" />
 
-                <Work image={ExpenseTracker} title="Expense Tracker" subTitle="UI/UX Design" text="This is a simple Expense Tracker design created for the React.js Bootcamp Held at EMEA College organized by TinkerHub EMEA. This was designed to give the participants a task for development." codeLink="https://www.figma.com/file/OpZVasP6AHifcys8CWcMPA/Expense-Tracker?node-id=2%3A2" />
+                <Work image={Piazza} title="Piazza Redesign" subTitle="UI/UX Design" text="Collaborative platform for students and instructors to share knowledge, homeworks and ask questions" codeLink="https://www.figma.com/file/OpZVasP6AHifcys8CWcMPA/Expense-Tracker?node-id=2%3A2" />
 
-                <Work image={Poster} title="Poster Design" subTitle="Graphic Design" text="Social Media banners created using various design tools such as Adobe Photoshop and Adobe Illustrator." codeLink="https://drive.google.com/drive/folders/17VEuYbOOSk0FqEyaVGmr7tFHKleC5xaN?usp=sharing" />*/}
+                <Work image={snapgram} title="Snapgram" subTitle="UI/UX Design" text="Connect, share, and engage with a vibrant community through photos and videos." codeLink="https://drive.google.com/drive/folders/17VEuYbOOSk0FqEyaVGmr7tFHKleC5xaN?usp=sharing" />*/}
 
             </div>
         </div>
