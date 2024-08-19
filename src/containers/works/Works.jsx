3869824adb2d395aa3792818f4409c53e7e307f0
Aylin Abdollahi <ailinabdollahi113@gmail.com>
2024-08-19 14:04:28 +0330

    h

--- src/containers/works/Works.jsx
+++ src/containers/works/Works.jsx
@@ -16,7 +16,7 @@ function Works() {
             <div className='works-container'>
                 <Work image={dAP} title="Doctor Appointment App" subTitle="Case Study" text="Simplifies booking and managing doctor appointments with reminders and secure scheduling."  />
 
-                <Work image={Travel} title="Travel Website" subTitle="UI/UX Design" text="Find and book exciting tours around the world, perfect for your next adventure." codeLink="https://www.figma.com/design/KdjjGgklTNUwbV8P58ij49/Travel?node-id=0-35&t=PGf2yKRfliAv70EX-1" />
+                <Work image={Travel} title="Travel Website" subTitle="UI/UX Design" text="Find and book exciting tours, perfect for your next adventure." codeLink="https://www.figma.com/design/KdjjGgklTNUwbV8P58ij49/Travel?node-id=0-35&t=PGf2yKRfliAv70EX-1" />
 
                 <Work image={MovieApp} title="MovieNet" subTitle="UI Design & Development" text="Discover and track movies and TV series with personalized recommendations." codeLink="https://www.figma.com/design/f0XaIW7rwN7PJPbF8S2yPC/Movie-App?node-id=0-1&t=wVDocizjjMihQLdm-1" liveLink="https://movienet-project.netlify.app/" />
 
