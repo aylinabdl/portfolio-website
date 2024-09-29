fc54e479bdac5107cdc85c8dda8ecde37e207360
Aylin Abdollahi <ailinabdollahi113@gmail.com>
2024-09-29 15:15:33 +0330

    movienet

--- src/containers/works/Works.jsx
+++ src/containers/works/Works.jsx
@@ -26,7 +26,7 @@ function Works() {
 
                 <Work image={Travel} title="Travel Website" subTitle="UI/UX Design" text="Find and book exciting tours in , perfect for your next adventure." figmaLink="https://www.figma.com/design/KdjjGgklTNUwbV8P58ij49/Travel?node-id=0-35&t=PGf2yKRfliAv70EX-1" />
 
-                <Work image={MovieApp} title="MovieNet" subTitle="UI Design & Development" text="Discover and track movies and TV series with personalized recommendations." figmaLink="https://www.figma.com/design/f0XaIW7rwN7PJPbF8S2yPC/Movie-App?node-id=0-1&t=wVDocizjjMihQLdm-1" liveLink="https://movienet-project.netlify.app/" />
+                <Work image={MovieApp} title="MovieNet" subTitle="UI Design & Development" text="Discover and track movies and TV series with personalized recommendations." liveLink="https://movienet-project.netlify.app/" />
 
             </div>
         </div>
