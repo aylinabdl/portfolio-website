ac4a04864d56331e32894bee29b4d43af4298404
Aylin Abdollahi <ailinabdollahi113@gmail.com>
2025-01-05 16:33:20 +0330

    new project

--- src/containers/works/Works.jsx
+++ src/containers/works/Works.jsx
@@ -28,8 +28,9 @@ function Works() {
             <div className='works-container'>
                 <Work image={dAP} title="Visitsho App" subTitle="Case Study" text="Simplifies booking and managing doctor appointments with reminders and secure scheduling."  caseStudyImage={viz}  />
                 <Work image={piazza} title="Piazza Redesign" subTitle="Case Study" text="Improve the usability of Piazza, focusing on a better posting flow & better accessibility to class sections" caseStudyImage={pics}  />
-               <Work image={Travel} title="Travia" subTitle="UI/UX Design" text="This project aims to simplify finding, booking, and managing holiday stays by tackling key challenges in online accommodation booking.." figmaLink="https://www.figma.com/design/CiT5qGD4JJdaMaxR8LxfI9/Travia?node-id=281-3486&t=ax0o4ZsiD3LjnZiQ-1"/>
+               
                 <Work image={Rahnama} title="Rahnama Website" subTitle="UI/UX Design" text="An E-Commerce website for Rahnama, created for Kanoon Informatics Company." caseStudyImage={Rahnamade}/>
+                <Work image={Travel} title="Travia" subTitle="UI/UX Design" text="This project aims to simplify finding, booking, and managing holiday stays by tackling key challenges in online accommodation booking.." figmaLink="https://www.figma.com/design/CiT5qGD4JJdaMaxR8LxfI9/Travia?node-id=281-3486&t=ax0o4ZsiD3LjnZiQ-1"/>
                 <Work image={FinApp} title="Finance App" subTitle="UI/UX Design" text="Manage all your bank accounts, view balances, and track expenses with an intuitive and secure interface." caseStudyImage={finn} figmaLink="https://www.figma.com/design/ywm6BmEeu66NZqIp5CS8Bk/Finance-Application?node-id=0-1&t=uuwlcsu5JCQ1fqBa-1"/>
                 
                 
